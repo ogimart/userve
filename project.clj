71da1ba7fd924bf5bcc719543d8ac999f4b0677a
Ogi Martinovic <ogimart@gmail.com>
2019-04-12 15:27:57 -0400

    update version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject userve/lein-template "0.1.0-beta"
+(defproject userve/lein-template "0.1.0"
   :description "Leiningen Miscroservice Template"
   :url "https://github.com/ogimart/userve"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
